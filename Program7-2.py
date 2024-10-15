9e28be5f5f1ae28ef75c465fee9a6db3723c51f6
Benjamin Johnson <bjohnson@fcusd.org>
2024-10-14 20:58:02 -0700

    Update and rename Program7-1.py to Program7-2.py

---
+++ Program7-2.py
@@ -0,0 +1,23 @@
+## Problem 1 ##
+# - Write a loop that calculates the sum of numbers 1 to 100
+
+
+
+## Problem 2 ##
+# - Write a loop that continues until the user types "exit"
+
+
+
+
+## Problem 3 ##
+#Please write a script that: 
+# - Uses a for loop
+
+
+
+## Problem 4 ##
+#Please write a script that: 
+# - Uses a while loop
+
+
+
